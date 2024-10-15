435f720c65e38e96f2b3cd655a1b6a47859088f8
algowhiz <algowhiz@gmail.com>
2024-10-15 15:29:57 +0530

    imagecrousal and items .js changes

--- pages/components/ImageCrousel.js
+++ pages/components/ImageCrousel.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/css';
@@ -6,49 +6,51 @@ import 'swiper/css/pagination';
 import Image from 'next/image';
 import Shimmer from './Shimmer';
 
-const ImageCrousel = ({images}) => {
+const ImageCrousel = ({ images }) => {
   return (
-    <div  className=" h-[50vh] md:h-[30vh] lg:h-[70vh]  cursor-pointer relative">
-         {images.length > 0 ? (
-          <Swiper
-            modules={[Autoplay, Pagination]} 
-            autoplay={{ delay: 3000 }} 
-            pagination={{ clickable: true }} 
-            spaceBetween={30}
-            slidesPerView={1}
-            loop={true}
-            className="h-full"
-          >
-            {images.map((image, index) => (
-              <SwiperSlide key={index}>
-                <div className="w-full hidden md:flex h-full  items-center justify-center">
-                  <Image
-                    src={image?.imageUrl}
-                    alt={`Carousel Image ${index}`}
-                    layout="fill"
-                    objectFit="cover"
-                    className="rounded-md "
-                  />
-                </div>
-                <div className=" md:hidden object-cover h-fit w-fit">
-                  <Image
-                    src={image?.mobileImageUrl}
-                    alt={`Carousel Image ${index}`}
-                    layout="fill"
-                    style={{ objectFit: 'cover' }}
-                    className="rounded-md "
-                  />
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        ) : (
-          <div className="flex items-center justify-center h-full">
-            <Shimmer type="carousel" />{/* <p>Loading carousel images...</p> */}
-          </div>
-        )}
+    <div className="h-[50vh] md:h-[30vh] lg:h-[70vh] cursor-pointer relative">
+      {images?.length > 0 ? (
+        <Swiper
+          modules={[Autoplay, Pagination]}
+          autoplay={{ delay: 3000 }}
+          pagination={{ clickable: true }}
+          spaceBetween={30}
+          slidesPerView={1}
+          loop={true}
+          className="h-full"
+        >
+          {images.map((image, index) => (
+            <SwiperSlide key={index}>
+              {/* Desktop View */}
+              <div className="w-full hidden md:flex h-full items-center justify-center">
+                <Image
+                  src={image?.imageUrl}
+                  alt={`Carousel Image ${index}`}
+                  fill
+                  style={{ objectFit: 'cover' }}
+                  className="rounded-md"
+                />
+              </div>
+              {/* Mobile View */}
+              <div className="md:hidden object-cover h-fit w-fit">
+                <Image
+                  src={image?.mobileImageUrl}
+                  alt={`Carousel Image ${index}`}
+                  fill
+                  style={{ objectFit: 'cover' }}
+                  className="rounded-md"
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <div className="flex items-center justify-center h-full">
+          <Shimmer type="carousel" /> {/* Fallback loading shimmer */}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ImageCrousel
+export default ImageCrousel;
