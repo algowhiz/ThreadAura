4ff8160a8f1c6badb737dc4b599740970036ddd0
algowhiz <algowhiz@gmail.com>
2024-10-15 21:57:12 +0530

    ui bug fix in category heigh of images

--- pages/components/Item.js
+++ pages/components/Item.js
@@ -13,7 +13,7 @@ const Item = ({ sortedProducts, slug }) => {
                 <div className="block relative rounded overflow-hidden w-full h-[60%]">
                   <img
                     alt="ecommerce"
-                    className="m-auto h-[30vh] md:h-[50vh] block"
+                    className="m-auto h-[30vh] md:h-[20vh] block"
                     src={product?.img}
                   />
                 </div>
