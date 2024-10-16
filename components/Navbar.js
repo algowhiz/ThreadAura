55119e04350977a3d54ffa4a25c49a363ca477fb
algowhiz <algowhiz@gmail.com>
2024-10-16 10:09:44 +0530

    navbar ui change

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,32 +8,38 @@ import { PiCheckFatFill } from "react-icons/pi";
 import { BsFillCartDashFill } from "react-icons/bs";
 import { IoMenu } from "react-icons/io5";
 import { useRouter } from 'next/router';
-
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { FaTshirt } from "react-icons/fa";
+import { RiShirtFill } from "react-icons/ri";
+import { GiHoodie, GiShirt, GiArmoredPants, GiShorts, GiTravelDress, GiRunningShoe } from "react-icons/gi";
+import { PiPantsFill } from "react-icons/pi";
+import { FaBagShopping, FaRedhat } from "react-icons/fa6";
+import { MdSmartToy } from "react-icons/md";
 
 const categories = {
   womens: [
     {
       name: "TOPWEAR",
       items: [
-        { name: "T-Shirts", slug: "women-oversize-tshirt" },
-        { name: "Shirts", slug: "womens-shirts" },
-        { name: "Hoodies", slug: "hoodies-sweatsshirts" },
-        { name: "Sweatshirts", slug: "hoodies-sweatsshirts" }
+        { name: "T-Shirts", slug: "women-oversize-tshirt", icon: <FaTshirt /> },
+        { name: "Shirts", slug: "womens-shirts", icon: <RiShirtFill /> },
+        { name: "Hoodies", slug: "hoodies-sweatsshirts", icon: <GiHoodie /> },
+        { name: "Sweatshirts", slug: "hoodies-sweatsshirts", icon: <GiShirt /> }
       ]
     },
     {
       name: "BOTTOMWEAR",
       items: [
-        { name: "Jeans", slug: "womens-jeans" },
-        { name: "pants", slug: "women-pants" },
-        { name: "Shorts", slug: "womens-shorts" },
-        { name: "joggers", slug: "women-joggers" }
+        { name: "Jeans", slug: "womens-jeans", icon: <PiPantsFill /> },
+        { name: "pants", slug: "women-pants", icon: <PiPantsFill /> },
+        { name: "Shorts", slug: "womens-shorts", icon: <GiShorts /> },
+        { name: "joggers", slug: "women-joggers", icon: <GiArmoredPants /> }
       ]
     },
     {
       name: "ACCESSORIES",
       items: [
-        { name: "Bags", slug: "bags" },
+        { name: "Bags", slug: "bags", icon: <FaBagShopping /> },
       ]
     }
   ],
@@ -41,26 +47,26 @@ const categories = {
     {
       name: "TOPWEAR",
       items: [
-        { name: "T-Shirts", slug: "t-shirts" },
-        { name: "Shirts", slug: "shirts" },
-        { name: "Hoodies", slug: "hoodies" },
-        { name: "Sweatshirts", slug: "sweatshirts" }
+        { name: "T-Shirts", slug: "t-shirts", icon: <FaTshirt /> },
+        { name: "Shirts", slug: "shirts", icon: <RiShirtFill /> },
+        { name: "Hoodies", slug: "hoodies", icon: <GiHoodie /> },
+        { name: "Sweatshirts", slug: "sweatshirts", icon: <GiShirt /> }
       ]
     },
     {
       name: "BOTTOMWEAR",
       items: [
-        { name: "Jeans", slug: "jeans" },
-        { name: "Joggers", slug: "joggers" },
-        { name: "Shorts", slug: "shorts" },
-        { name: "Trousers", slug: "trousers" }
+        { name: "Jeans", slug: "jeans", icon: <PiPantsFill /> },
+        { name: "Joggers", slug: "joggers", icon: <GiArmoredPants /> },
+        { name: "Shorts", slug: "shorts", icon: <GiShorts /> },
+        { name: "Trousers", slug: "trousers", icon: <GiArmoredPants /> }
       ]
     },
     {
       name: "SNEAKERS",
       items: [
-        { name: "Running Shoes", slug: "running-shoes" },
-        { name: "Casual Shoes", slug: "casual-shoes" }
+        { name: "Running Shoes", slug: "running-shoes", icon: <GiRunningShoe /> },
+        { name: "Casual Shoes", slug: "casual-shoes", icon: <GiRunningShoe /> }
       ]
     }
   ],
@@ -68,18 +74,18 @@ const categories = {
     {
       name: "CLOTHING",
       items: [
-        { name: "T-Shirts", slug: "kids-t-shirts" },
-        { name: "Pants", slug: "kids-pants" },
-        { name: "Shorts", slug: "kids-shorts" },
-        { name: "Dresses", slug: "kids-dresses" }
+        { name: "T-Shirts", slug: "kids-t-shirts", icon: <FaTshirt /> },
+        { name: "Pants", slug: "kids-pants", icon: <PiPantsFill /> },
+        { name: "Shorts", slug: "kids-shorts", icon: <GiShorts /> },
+        { name: "Dresses", slug: "kids-dresses", icon: <GiTravelDress /> }
       ]
     },
     {
       name: "ACCESSORIES",
       items: [
-        { name: "Caps", slug: "kids-caps" },
-        { name: "Bags", slug: "kids-bags" },
-        { name: "Toys", slug: "kids-toys" }
+        { name: "Caps", slug: "kids-caps", icon: <FaRedhat /> },
+        { name: "Bags", slug: "kids-bags", icon: <FaBagShopping /> },
+        { name: "Toys", slug: "kids-toys", icon: <MdSmartToy /> }
       ]
     }
   ]
@@ -88,16 +94,21 @@ const categories = {
 
 const Navbar = ({ user, setUser, cart, addToCart, removeFromCart, clearCart, subTotal }) => {
   const [toggle, setToggle] = useState(false);
+  const [navToggle, setnavToggle] = useState(false);
   const [dropDown, setDropDown] = useState(false);
   const [activeCategory, setActiveCategory] = useState('mens');
   const [dropdownVisible, setDropdownVisible] = useState(null);
   const [scrolled, setScrolled] = useState(false);
+  const [accordion, setAccordion] = useState("");
   const router = useRouter();
 
   let hideDropdownTimeout = null;
 
   const handleToggle = () => {
     setToggle(!toggle);
+  };
+  const handleNavToggle = () => {
+    setnavToggle(!navToggle);
   };
 
   const handelLogout = () => {
@@ -135,6 +146,13 @@ const Navbar = ({ user, setUser, cart, addToCart, removeFromCart, clearCart, sub
     setScrolled(window.scrollY > 1);
   };
 
+  const handelCategoryAccordion = (category) => {
+    if (accordion === category)
+      setAccordion("");
+    else
+      setAccordion(category);
+  }
+
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
     return () => {
@@ -151,6 +169,55 @@ const Navbar = ({ user, setUser, cart, addToCart, removeFromCart, clearCart, sub
     }
   }, [router.query.category]);
 
+  const mobileDropDowncomponent = () => {
+    return <div
+      onMouseEnter={toggleDropDown}
+      onMouseLeave={toggleHideDropDown}
+      className="absolute  truncate -right-7  mt-2 bg-white text-black shadow-lg rounded-md p-2"
+    >
+      <ul>
+        <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">
+          <Link href="/myaccount">My Account</Link>
+        </li>
+        <hr />
+        <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">
+          <Link href={`/orders/${localStorage.getItem("thread_aura__id")}`}>Orders</Link>
+        </li>
+        <hr />
+        <li
+          className="py-1 px-2 hover:bg-gray-100 cursor-pointer"
+          onClick={handelLogout}
+        >
+          Logout
+        </li>
+      </ul>
+    </div>
+  }
+
+  const dropDowncomponent = () => {
+    return <div
+      onMouseEnter={toggleDropDown}
+      onMouseLeave={toggleHideDropDown}
+      className="absolute truncate whitespace-nowrap left-1/2 transform -translate-x-1/2 mt-2 bg-white text-black shadow-lg rounded-md p-2"
+    >
+      <ul>
+        <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">
+          <Link href="/myaccount">My Account</Link>
+        </li>
+        <hr />
+        <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">
+          <Link href={`/orders/${localStorage.getItem("thread_aura__id")}`}>Orders</Link>
+        </li>
+        <hr />
+        <li
+          className="py-1 px-2 hover:bg-gray-100 cursor-pointer"
+          onClick={handelLogout}
+        >
+          Logout
+        </li>
+      </ul>
+    </div>
+  }
 
   return (
     <div>
@@ -208,28 +275,7 @@ const Navbar = ({ user, setUser, cart, addToCart, removeFromCart, clearCart, sub
                 />
 
                 {dropDown && (
-                  <div
-                    onMouseEnter={toggleDropDown}
-                    onMouseLeave={toggleHideDropDown}
-                    className="absolute truncate whitespace-nowrap left-1/2 transform -translate-x-1/2 mt-2 bg-white text-black shadow-lg rounded-md p-2"
-                  >
-                    <ul>
-                      <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">
-                        <Link href="/myaccount">My Account</Link>
-                      </li>
-                      <hr />
-                      <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">
-                        <Link href={`/orders/${localStorage.getItem("thread_aura__id")}`}>Orders</Link>
-                      </li>
-                      <hr />
-                      <li
-                        className="py-1 px-2 hover:bg-gray-100 cursor-pointer"
-                        onClick={handelLogout}
-                      >
-                        Logout
-                      </li>
-                    </ul>
-                  </div>
+                  dropDowncomponent()
                 )}
               </div>
             )}
@@ -246,7 +292,7 @@ const Navbar = ({ user, setUser, cart, addToCart, removeFromCart, clearCart, sub
 
         </div>
         <div className="container md:hidden flex mx-auto  flex-wrap flex-row justify-between md:flex-row p-3 md:p-1 items-center">
-          <IoMenu size={30} />
+          <IoMenu onClick={handleNavToggle} size={30} />
           <div className=" flex justify-center items-center z-50">
             <div className="absolute left-1/2 transform -translate-x-1/2">
               <img src="/final2-logo.png" className="w-28 h-28 object-contain rounded-full" alt="Logo" />
@@ -271,28 +317,7 @@ const Navbar = ({ user, setUser, cart, addToCart, removeFromCart, clearCart, sub
                 />
 
                 {dropDown && (
-                  <div
-                    onMouseEnter={toggleDropDown}
-                    onMouseLeave={toggleHideDropDown}
-                    className="absolute  truncate -right-7  mt-2 bg-white text-black shadow-lg rounded-md p-2"
-                  >
-                    <ul>
-                      <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">
-                        <Link href="/myaccount">My Account</Link>
-                      </li>
-                      <hr />
-                      <li className="py-1 px-2 hover:bg-gray-100 cursor-pointer">
-                        <Link href={`/orders/${localStorage.getItem("thread_aura__id")}`}>Orders</Link>
-                      </li>
-                      <hr />
-                      <li
-                        className="py-1 px-2 hover:bg-gray-100 cursor-pointer"
-                        onClick={handelLogout}
-                      >
-                        Logout
-                      </li>
-                    </ul>
-                  </div>
+                  mobileDropDowncomponent()
                 )}
               </div>
             )}
@@ -339,35 +364,6 @@ const Navbar = ({ user, setUser, cart, addToCart, removeFromCart, clearCart, sub
             </div>
           </div>
         </div>
-        <div>
-          <nav className="md:ml-auto md:hidden bg-white text-black border-4 border-gray-400 font-bold md:mr-auto flex flex-wrap items-center justify-between text-base px-4 py-2">
-            <Link href={"/categories?category=womens"}>
-              <p
-                className={`mx-auto w-full px-4 py-2  hover:text-blue-500 cursor-pointer ${activeCategory === 'women' ? 'text-blue-500' : ''}`}
-                onClick={() => handleCategoryChange('women')}
-              >
-                Women
-              </p>
-            </Link>
-            <Link href={"/categories?category=mens"}>
-              <p
-                className={`mx-auto  w-full px-4 py-2  hover:text-blue-500 cursor-pointer ${activeCategory === 'men' ? 'text-blue-500' : ''}`}
-                onClick={() => handleCategoryChange('men')}
-              >
-                Men
-              </p>
-            </Link>
-            <Link href={"/categories?category=kids"}>
-              <p
-                className={`mx-auto  w-full px-4 py-2 hover:text-blue-500 cursor-pointer ${activeCategory === 'kids' ? 'text-blue-500' : ''}`}
-                onClick={() => handleCategoryChange('kids')}
-              >
-                Kids
-              </p>
-            </Link>
-          </nav>
-        </div>
-
 
       </header>
 
@@ -405,6 +401,57 @@ const Navbar = ({ user, setUser, cart, addToCart, removeFromCart, clearCart, sub
         <div className="w-full flex mt-8  gap-2">
           <Link href={'/checkouts'}><button className="flex justify-center items-center whitespace-nowrap gap-2 mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-2 md:px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"><PiCheckFatFill color='white' /> Checkout</button></Link>
           <button onClick={clearCart} className="flex justify-center gap-2 items-center mx-auto mt-16 text-white whitespace-nowrap bg-indigo-500 border-0 py-2 px-2 md:px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"><BsFillCartDashFill color='white' /> Clear Cart</button>
+        </div>
+      </div>
+      {/* navbar  Sidebar  */}
+      <div className={`sidebar md:hidden absolute overflow-y-scroll w-72 md:w-96 left-0 top-0  text-black bg-white p-4 transform transition-transform duration-700 ease-in-out ${navToggle ? 'left-0' : '-translate-x-full'} shadow-2xl z-50 rounded-md h-[100vh]`}>
+        <p className="text-xl font-bold">This is Shopping Cart</p>
+        <span className="absolute top-3 right-3 font-semibold cursor-pointer hover:text-blue-500" onClick={handleNavToggle}>
+          <IoClose size={20} />
+        </span>
+        <div className='w-full'>
+          <nav className="md:ml-auto md:hidden bg-white text-black  font-bold md:mr-auto flex flex-nowrap items-center justify-between text-base px-4 py-2">
+            <Link href={"/categories?category=womens"}>
+              <p
+                className={`mx-auto w-full px-4 py-2  hover:text-blue-500 cursor-pointer ${activeCategory === 'womens' ? 'text-blue-500' : ''}`}
+                onClick={() => handleCategoryChange('women')}
+              >
+                Women
+              </p>
+            </Link>
+            <Link href={"/categories?category=mens"}>
+              <p
+                className={`mx-auto  w-full px-4 py-2  hover:text-blue-500 cursor-pointer ${activeCategory === 'mens' ? 'bg-blue-500 text-white py-2 rounded-lg px-4 ' : ''}`}
+                onClick={() => handleCategoryChange('men')}
+              >
+                Men
+              </p>
+            </Link>
+            <Link href={"/categories?category=kids"}>
+              <p
+                className={`mx-auto  w-full px-4 py-2 hover:text-blue-500 cursor-pointer ${activeCategory === 'kids' ? 'text-blue-500' : ''}`}
+                onClick={() => handleCategoryChange('kids')}
+              >
+                Kids
+              </p>
+            </Link>
+          </nav>
+          <div className="w-full mt-4 flex flex-col">
+            {categories[activeCategory]?.map((category, index) => (
+              <div key={index} className="flex flex-col space-y-2">
+                <p className="text-lg font-semibold bg-gray-300 p-2 mb-2 flex justify-between text-gray-800" onClick={() => handelCategoryAccordion(category?.name)} > <p> {category?.name}</p> <p> {accordion == category.name ? <IoIosArrowUp /> : <IoIosArrowDown />}</p> </p>
+                {accordion === category?.name && <ul className="pl-4">
+                  {category?.items?.map((subcategory, subIndex) => (
+                    <li key={subIndex} className="py-1 text-gray-700  hover:text-blue-600">
+                      <Link className='flex justify-center items-center gap-2' href={`/category?slug=${subcategory?.slug}&category=${activeCategory}`}>
+                        <p>{subcategory?.name}</p> <p>{subcategory?.icon}</p>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
